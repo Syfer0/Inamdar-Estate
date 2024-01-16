c12b6b02d42c872a6cc64a9cf6f022a787fa7d76
Arman <125900320+Syfer0@users.noreply.github.com>
2024-01-16 19:11:32 +0530

    first commit

---
+++ api/index.js
@@ -0,0 +1,58 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
+import cookieParser from "cookie-parser";
+import path from "path";
+import { createProxyMiddleware } from "http-proxy-middleware";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const __dirname = path.resolve();
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+// Define a proxy for the Vite development server
+const viteProxy = createProxyMiddleware("/client", {
+  target: "http://localhost:5173",
+  changeOrigin: true,
+});
+app.use(viteProxy);
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000!");
+});
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
+
+// Server the index.html from the Vite development server
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
---
+++ client/src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBiAaTLc9eZl3OUm7YZwH3G7HnrBpAuyhk",
+  authDomain: "mern-estate-754fc.firebaseapp.com",
+  projectId: "mern-estate-754fc",
+  storageBucket: "mern-estate-754fc.appspot.com",
+  messagingSenderId: "141617998040",
+  appId: "1:141617998040:web:a2a63970e4a165d1598fad",
+  measurementId: "G-126YSTBYH4",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
