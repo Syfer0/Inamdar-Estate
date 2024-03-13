b9bc58c1ccd556b3c500b1d8ef322a5cace029eb
Arman <125900320+Syfer0@users.noreply.github.com>
2024-03-13 16:31:37 +0530

    update the types  check

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,18 +1,18 @@
-import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import userRouter from "./routes/user.route.js";
-import authRouter from "./routes/auth.route.js";
-import listingRouter from "./routes/listing.route.js";
-import cookieParser from "cookie-parser";
-import path from "path";
-import { createProxyMiddleware } from "http-proxy-middleware";
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
+import cookieParser from 'cookie-parser';
+import path from 'path';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log("Connected to MongoDB!");
+    console.log('Connected to MongoDB!');
   })
   .catch((err) => {
     console.log(err);
@@ -26,30 +26,30 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Define a proxy for the Vite development server
-const viteProxy = createProxyMiddleware("/client", {
-  target: "http://localhost:5173",
+const viteProxy = createProxyMiddleware('/client', {
+  target: 'http://localhost:5173',
   changeOrigin: true,
 });
 app.use(viteProxy);
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000!");
+  console.log('Server is running on port 3000!');
 });
 
-app.use("/api/user", userRouter);
-app.use("/api/auth", authRouter);
-app.use("/api/listing", listingRouter);
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
 
 // Server the index.html from the Vite development server
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || "Internal Server Error";
+  const message = err.message || 'Internal Server Error';
   return res.status(statusCode).json({
     success: false,
     statusCode,
