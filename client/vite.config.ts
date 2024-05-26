f84a7823452ef11b376161a5aab7121430ad2d5d
Arman <125900320+Syfer0@users.noreply.github.com>
2024-05-26 14:05:34 +0530

    reconfig-vercel-TDD-7 -- backend Done✅Front-end

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,22 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+
+// export default defineConfig({
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: "http://localhost:3000",
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, "/api"),
+//       },
+//     },
+//   },
+//   plugins: [react()],
+// });
+//production
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, "/api"),
-      },
-    },
-  },
   plugins: [react()],
 });
