f79340328688f77d4bec57302698f24c34c0e39f
Arman <125900320+Syfer0@users.noreply.github.com>
2024-05-26 13:16:02 +0530

    reconfig-vercel-TDD-3

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "inamdar-estate-mc8t.vercel.app",
-        secure: false,
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, "/api"),
       },
     },
   },
-
   plugins: [react()],
 });
